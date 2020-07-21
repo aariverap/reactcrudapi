394c51596bce378db547d159c9cf1766f364807c
aariverap <aariverap@unal.edu.co>
2020-07-21 10:34:23 -0500

    first commit

---
+++ precache-manifest.edb72a31ca661d18c745a4e70626aa30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02cc22a333b2ac961d51d8f346c6cd4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8bf8a0c4811de9009cb",
+    "url": "/static/css/main.71bd5e9e.chunk.css"
+  },
+  {
+    "revision": "a60f5d68c1cfa2c39db0",
+    "url": "/static/js/2.69b2ec3e.chunk.js"
+  },
+  {
+    "revision": "de1cedeacf4641d6277b42334cfacb1d",
+    "url": "/static/js/2.69b2ec3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8bf8a0c4811de9009cb",
+    "url": "/static/js/main.315ce20c.chunk.js"
+  },
+  {
+    "revision": "88a2a38fad697d24ce4c",
+    "url": "/static/js/runtime-main.abf55560.js"
+  }
+]);
